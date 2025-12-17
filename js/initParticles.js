6278651e95ffded12fbd486ceb8e626513b7d34d
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2025-12-18 00:44:55 +0100

    Update cpjs from 3.8.2 to 4.1.2

--- js/initParticles.js
+++ js/initParticles.js
@@ -1,19 +1,20 @@
+import CanvasParticles from 'https://cdn.jsdelivr.net/npm/canvasparticles-js@4.1/dist/index.min.mjs'
+
 // Init particles
 const selector = '#canvas-particles'
 const options = {
   background: 'linear-gradient(115deg, #354089, var(--black))',
-  framesPerUpdate: 1,
-  resetOnResize: false,
 
   mouse: {
     interactionType: 2,
-    connectDistMult: 0.8,
+    connectDistMult: 1.25,
     distRatio: 1,
   },
 
   particles: {
     color: '#88c8ff',
-    ppm: 70,
+    ppm: 140,
+    connectDistance: 110,
     relSpeed: 0.8,
     rotationSpeed: 1,
   },
